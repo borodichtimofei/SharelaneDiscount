23693aae6a6ef95519649aa963239034c623ea88
Timofei <borodich.timofei@gmail.com>
2022-03-03 20:35:10 +0300

    Initial commit

---
+++ src/test/java/DiscountTest.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class DiscountTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("test");
+        driver.findElement(By.name("last_name")).sendKeys("test");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("123456789");
+        driver.findElement(By.name("password2")).sendKeys("123456789");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String login = driver.findElement(By.xpath("//*[contains(text(), 'Email')]/..//b")).getText();
+        String password = driver.findElement(By.xpath("//*[contains(text(), 'Password')]/..//td[2]")).getText();
+        driver.get("https://sharelane.com/cgi-bin/show_book.py?book_id=2");
+        driver.findElement(By.name("email")).sendKeys(login);
+        driver.findElement(By.name("password")).sendKeys("1111");
+        driver.findElement(By.cssSelector("[value=Login]")).click();
+        driver.get("https://sharelane.com/cgi-bin/show_book.py?book_id=2");
+        driver.findElement(By.cssSelector("[href='./add_to_cart.py?book_id=2']")).click();
+        driver.get("https://sharelane.com/cgi-bin/shopping_cart.py");
+    }
+
+    @Test
+    public void discountShouldNullPercentValue19() {
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("19");
+        driver.findElement(By.cssSelector("[value=Update]")).click();
+        String price = driver.findElement(By.xpath("//tr[2]//td[7]")).getText();
+        assertEquals(price, "190", "price not correct");
+    }
+
+    @Test
+    public void discountShouldTwoPercentValue20() {
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("20");
+        driver.findElement(By.cssSelector("[value=Update]")).click();
+        String price = driver.findElement(By.xpath("//tr[2]//td[7]")).getText();
+        assertEquals(price, "196", "price not correct");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
